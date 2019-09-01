1ea44edbb8a69894666737a2e935f0f3b1bbe9ff
Michał <michal@dymecki.com>
2019-09-01 19:20:00 +0200

    Add Card element

--- src/Views/Web/resources/views/races/_race.blade.php
+++ src/Views/Web/resources/views/races/_race.blade.php
@@ -1,35 +1,45 @@
-<h4>Race {{ substr($race->id(), 0, 8) }}</h4>
 
-<table class="table _table-striped table-sm">
-    <thead>
-        <tr>
-            <th scope="col">Horse id</th>
-            <th scope="col">Position</th>
-            <th scope="col">Distance covered</th>
-            <th scope="col">Time</th>
-        </tr>
-    </thead>
-    <tbody>
+<!--<h4>Race {{ substr($race->id(), 0, 8) }} <span class="badge badge-secondary">{{ $race->time() }}</span></h4>-->
 
-        @foreach ($race->horses() as $horse)
-        <tr>
-            <td>{{ substr($horse->horse()->id(), 0, 8) }}</td>
-            <td></td>
-            <td>
-                <div class="progress">
-                    <div
-                        class="progress-bar progress-bar-striped bg-success"
-                        role="progressbar"
-                        style="width: {{ $horse->stats()->distance() }}%"
-                        aria-valuenow="{{ $horse->stats()->distance() }}"
-                        aria-valuemin="0"
-                        aria-valuemax="100">
-                    </div>
-                </div>
-            </td>
-            <td>{{ $horse->stats()->time() }}</td>
-        </tr>
-        @endforeach
+<div class="card text-white bg-dark mb-3">
+    <div class="card-header">
+        Race {{ substr($race->id(), 0, 8) }} <span class="badge badge-secondary">{{ $race->time() }}</span>
+    </div>
+    <div class="card-body" style="background-color: transparent">
+        <h5 class="card-title">Special title treatment</h5>
+        
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
 
-    </tbody>
-</table>
+        <table class="table table-striped table-sm table-borderless table-dark">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Horse</th>
+                    <th scope="col">Position</th>
+                    <th scope="col">Distance covered</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($race->horses() as $horse)
+                <tr>
+                    <td>{{ substr($horse->horse()->id(), 0, 8) }}</td>
+                    <td></td>
+                    <td>
+                        <div class="progress">
+                            <div
+                                class="progress-bar progress-bar-striped bg-success _progress-bar-animated"
+                                role="progressbar"
+                                style="width: {{ $horse->stats()->distanceCovered()->value() / $race->distance()->value() * 100 }}%"
+                                aria-valuenow="{{ $horse->stats()->distanceCovered() }}"
+                                aria-valuemin="0"
+                                aria-valuemax="100">{{ $horse->stats()->distanceCovered() }}
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+</div>
