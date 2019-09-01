f8a8a4d79d125f15a2ef31e57736921a46e4c151
Michał <michal@dymecki.com>
2019-09-02 01:48:15 +0200

    Move logic from routes to the service

--- src/Views/Web/routes/web.php
+++ src/Views/Web/routes/web.php
@@ -3,49 +3,27 @@
 declare(strict_types = 1);
 
 use App\Application\Services\RaceService;
-//use App\Application\Services\HorseService;
-use App\Persistence\Dao\Mappers\RaceMapper;
 
 Route::get('/', function() {
-    $raceService = new RaceService();
-    $races       = $raceService->activeRaces();
-
-    $raceMapper = new RaceMapper($races);
-    $races      = $raceMapper->get();
-
-//    $horseService = new HorseService();
-//    $horses       = $horseService->getAll();
-//    var_dump($races);exit;
-
-    return view('main')->with('races', $races);
+    return view('main')->with('races', (new RaceService())->activeRaces());
 })->name('index');
 
 Route::get('races/{id}', function($id) {
-    $raceService = new RaceService();
-    $race        = $raceService->getById($id);
-
-    $raceMapper = new RaceMapper([$race]);
-    $race       = $raceMapper->get();
-//    var_dump($race);exit;
-
-    return view('races.show')->with('race', $race);
+    return view('races.show')->with('race', (new RaceService())->getById($id));
 })->name('races.show');
 
 Route::get('progress', function() {
-    $raceService = new RaceService();
-    $raceMapper  = new RaceMapper($raceService->activeRaces());
-    $races       = $raceMapper->get();
-
-    foreach ($races as $race) {
-        $race->runForSeconds(10);
-    }
+    (new RaceService())->updateRaces();
 
     return redirect()->route('index');
 })->name('progress');
 
 Route::get('create-race', function() {
-    $raceService = new RaceService();
-    $raceService->startNewRace();
+    (new RaceService())->startNewRace();
 
     return redirect()->route('index');
 })->name('create');
+
+Route::get('last-races', function() {
+    return view('races.last')->with('races', (new RaceService())->getLastRacesBestPositions());
+})->name('last-races');
