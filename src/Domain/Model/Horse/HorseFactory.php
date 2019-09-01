89a27394c80ef2fdf1a2d835fdd7b9f24b4bcb55
Michał <michal@dymecki.com>
2019-09-01 18:26:13 +0200

    Change random stats' values from int to float type

--- src/Domain/Model/Horse/HorseFactory.php
+++ src/Domain/Model/Horse/HorseFactory.php
@@ -10,12 +10,15 @@ use App\Domain\Model\Race\Stats\RunningHorseStats;
 
 final class HorseFactory
 {
-
     public static function make(): RunningHorse
     {
         $horse = new Horse(
             HorseId::init(),
-            HorseStats::create(rand(0, 10), rand(0, 10), rand(0, 10))
+            HorseStats::create(
+                rand(0, 10) / 10,
+                rand(0, 10) / 10,
+                rand(0, 10) / 10
+            )
         );
 
         return new RunningHorse($horse, RunningHorseStats::start());
