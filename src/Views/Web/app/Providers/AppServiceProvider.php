654e194d00f0a3e4a146689cbc02a4b067cfb555
Michał <michal@dymecki.com>
2019-09-02 16:25:46 +0200

    Call RaceService::canAddNewRace() on every page request in Blade

--- src/Views/Web/app/Providers/AppServiceProvider.php
+++ src/Views/Web/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Application\Services\RaceService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('layout', function($view) {
+            $view->with('canAddNewRace', (new RaceService())->canAddNewRace());
+        });
     }
 }
