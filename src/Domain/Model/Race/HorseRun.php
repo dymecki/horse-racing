3c09ebb3c8acc9dfc90a7a03fe771a8124c4c888
Michał <michal@dymecki.com>
2019-09-04 13:06:50 +0200

    Fix algorithm

--- src/Domain/Model/Race/HorseRun.php
+++ src/Domain/Model/Race/HorseRun.php
@@ -28,10 +28,38 @@ final class HorseRun
             Horse::create($data),
             HorseRunStats::create(
                 (float) $data->distance_covered,
-                $data->time,
+                (float) $data->time,
                 $data->horse_position ?? 0
             )
         );
+    }
+
+    public function runForSeconds(int $seconds, int $raceDistance = 1500): void
+    {
+        $time              = $this->stats->time()->value();
+        $fullSpeedDistance = $this->fullSpeedDistance()->value();
+        $slowSpeedDistance = $this->slowSpeed()->distance()->value();
+        $fullSpeed         = $this->horse->stats()->speed()->time()->value();
+        $fullSpeedSeconds  = $fullSpeedDistance / $fullSpeed;
+        $forwardTime       = $time + $seconds;
+
+        $coveredDistance = $forwardTime > $fullSpeedSeconds 
+            ? $fullSpeedDistance + ($forwardTime - $fullSpeedSeconds) * $slowSpeedDistance
+            : $forwardTime * $fullSpeed;
+
+        $time = $forwardTime;
+
+        if ($coveredDistance > $maxDistance) {
+            $time            -= $time * ($coveredDistance - $maxDistance) / $coveredDistance;
+            $coveredDistance = $maxDistance;
+        }
+
+        $this->stats->update(new Distance($coveredDistance), $time);
+    }
+
+    public function isStillGoing(Distance $raceDistance): bool
+    {
+        return $this->stats->distanceCovered() < $raceDistance;
     }
 
     public function horse(): Horse
@@ -44,89 +72,9 @@ final class HorseRun
         return $this->stats;
     }
 
-    // TODO: metoda nieużywana
-    public function move(): void
+    private function fullSpeedDistance(): Distance
     {
-        $this->stats->increaseDistance($this->speed()->distance());
-        $this->stats->updateTime();
-    }
-
-//    public function moveByTime(): void
-//    {
-//        $this->stats->increaseTime($this->speed()->secondsPerMeter()->value());
-//        $this->stats->increaseDistance(new Distance(1));
-//    }
-
-    public function speed(): Speed
-    {
-        return $this->isTired() ? $this->slowSpeed() : $this->horse->stats()->speed();
-    }
-
-    public function runForSeconds(int $seconds, int $raceDistance = 1500): void
-    {
-        $this->distanceAlgorithm($seconds, $raceDistance);
-//        exit;
-//
-//        $tmpDistance     = new Distance();
-//        $distanceCovered = $this->stats->distanceCovered();
-//
-////        for ($i = 0; $i < $seconds; $i++) {
-////            $oneSecondDistance = $this->checkIfTired($distanceCovered) ? $this->slowSpeed()->distance() : $this->horse->stats()->speed()->distance();
-////            $tmpDistance       = $tmpDistance->withAdd($oneSecondDistance);
-////            $distanceCovered   = $distanceCovered->withAdd($oneSecondDistance);
-////
-////            if ($distanceCovered->value() >= $raceDistance) {
-////                break;
-////            }
-////        }
-//        // =====================
-//
-//        $counter = 0;
-//
-//        while ($counter < $seconds && $distanceCovered->value() < $raceDistance) {
-//            $oneSecondDistance = $this->checkIfTired($distanceCovered) ? $this->slowSpeed()->distance() : $this->horse->stats()->speed()->distance();
-//            $tmpDistance       = $tmpDistance->withAdd($oneSecondDistance);
-//            $distanceCovered   = $distanceCovered->withAdd($oneSecondDistance);
-//
-//            $counter++;
-//        }
-//
-//        $this->stats->increase($tmpDistance, $seconds);
-    }
-
-    public function distanceAlgorithm($seconds = 10, int $maxDistance = 1500)
-    {
-        $time              = $this->stats->time()->value();
-        $fullSpeedDistance = $this->fullSpeedDistance()->value();
-        $fullSpeed         = $this->horse->stats()->speed()->time()->value();
-        $fullSpeedSeconds  = $fullSpeedDistance / $fullSpeed;
-        $forwardTime       = $time + $seconds;
-
-        if ($forwardTime > $fullSpeedSeconds) {
-            $slowSpeedTime    = $forwardTime - $fullSpeedSeconds;
-            $slowPartDistance = $slowSpeedTime * $this->slowSpeed()->distance()->value();
-            $coveredDistance  = $fullSpeedDistance + $slowPartDistance;
-//            $coveredDistance = $fullSpeedDistance + ($forwardTime - $fullSpeedSeconds) * $this->slowSpeed()->distance()->value();
-        }
-        else {
-            $coveredDistance = $forwardTime * $fullSpeed;
-        }
-
-        $time = $forwardTime;
-
-        if ($coveredDistance > $maxDistance) {
-            $diff            = $coveredDistance - $maxDistance;
-            $ratio           = $diff / $coveredDistance;
-            $coveredDistance = $maxDistance;
-            $time            = $time - $time * $ratio;
-        }
-
-        $this->stats->update(new Distance($coveredDistance), $time);
-    }
-
-    public function isStillGoing(Distance $raceDistance): bool
-    {
-        return $this->stats->distanceCovered() < $raceDistance;
+        return new Distance($this->horse->stats()->endurance()->value() * self::ENDURANCE_METERS);
     }
 
     private function slowSpeed(): Speed
@@ -137,20 +85,5 @@ final class HorseRun
     private function slownessFactor(): float
     {
         return 5 * $this->horse->stats()->strength()->value() * 8 / 100;
-    }
-
-    private function checkIfTired(Distance $distanceCovered): bool
-    {
-        return $distanceCovered > $this->fullSpeedDistance();
-    }
-
-    public function isTired(): bool
-    {
-        return $this->checkIfTired($this->stats()->distanceCovered());
-    }
-
-    public function fullSpeedDistance(): Distance
-    {
-        return new Distance($this->horse->stats()->endurance()->value() * self::ENDURANCE_METERS);
     }
 }
