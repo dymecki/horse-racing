b9f936d51cb67f72423ba7bec61bd6721b715fd9
Michał <michal@dymecki.com>
2019-09-05 19:15:16 +0200

    Remove UUID extension

--- db/migrations/20190828160005_create_races_table.php
+++ db/migrations/20190828160005_create_races_table.php
@@ -3,14 +3,13 @@
 declare(strict_types = 1);
 
 use Phinx\Migration\AbstractMigration;
-use Phinx\Util\Literal;
 
 class CreateRacesTable extends AbstractMigration
 {
     public function change()
     {
         $this->table('races', ['id' => false, 'primary_key' => ['race_id']])
-            ->addColumn('race_id', 'uuid', ['default' => Literal::from('uuid_generate_v4()')])
+            ->addColumn('race_id', 'uuid')
             ->addColumn('distance', 'integer')
             ->addColumn('created_at', 'timestamp', ['timezone' => true, 'default' => 'CURRENT_TIMESTAMP'])
             ->addColumn('updated_at', 'timestamp', ['timezone' => true, 'null' => true])
