40632c828c847cc05be306f4301400eff253f111
Michał <michal@dymecki.com>
2019-09-08 17:48:56 +0200

    Add RaceDtoIterator

---
+++ src/Domain/Model/Race/RaceDtoIterator.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Domain\Model\Race;
+
+use App\Application\DtoAssemblers\RaceDtoAssembler;
+use App\Application\DtoAssemblers\Dto\RaceDto;
+
+final class RaceDtoIterator implements \Iterator
+{
+    private $collection;
+    private $key;
+
+    public function __construct(RaceCollection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    public function current(): RaceDto
+    {
+        return (new RaceDtoAssembler($this->collection->get($this->key)))->dto();
+    }
+
+    public function key(): int
+    {
+        return $this->key;
+    }
+
+    public function next(): void
+    {
+        $this->key++;
+    }
+
+    public function rewind(): void
+    {
+        $this->key = 0;
+    }
+
+    public function valid(): bool
+    {
+        return $this->collection->isset($this->key);
+    }
+}
