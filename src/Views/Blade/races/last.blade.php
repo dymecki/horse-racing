dc7faf89d04fdb86f3c21e23a82e07d537a27e5e
Michał <michal@dymecki.com>
2019-09-08 18:20:45 +0200

    Reformat Blade views

--- src/Views/Blade/races/last.blade.php
+++ src/Views/Blade/races/last.blade.php
@@ -4,67 +4,66 @@
 
 @section('content')
 
-<h1>Last 5 races</h1>
+    <h1>Last 5 races</h1>
 
-<p>Best three horses in every of last five races.</p>
+    <p>Best three horses in every of last five races.</p>
 
-@if ($races)
-<div class="accordion" id="last-races">
+    @if ($races)
+        <div class="accordion" id="last-races">
 
-    @foreach ($races as $race)
-    <div class="card">
-        <div
-            class="card-header"
-            id="heading-{{ $race->name }}"
-            data-toggle="collapse"
-            data-target="#collapse-{{ $race->name }}"
-            aria-expanded="true"
-            aria-controls="collapse-{{ $race->name }}"
-            style="cursor: pointer;"
-            >
-            <h2 class="mb-0 align-baseline">
-                <span class="h6">Race {{ $race->name }} - {{ $race->distance }}m</span>
-            </h2>
+            @foreach ($races as $race)
+                <div class="card">
+                    <div
+                            class="card-header"
+                            id="heading-{{ $race->name }}"
+                            data-toggle="collapse"
+                            data-target="#collapse-{{ $race->name }}"
+                            aria-expanded="true"
+                            aria-controls="collapse-{{ $race->name }}"
+                            style="cursor: pointer;"
+                    >
+                        <h2 class="mb-0 align-baseline">
+                            <span class="h6">Race {{ $race->name }} - {{ $race->distance }}m</span>
+                        </h2>
+                    </div>
+
+                    <div
+                            id="collapse-{{ $race->name }}"
+                            class="collapse @if ($loop->first) show @endif"
+                            aria-labelledby="heading-{{ $race->name }}"
+                            data-parent="#last-races"
+                    >
+                        <div class="card-body">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Position</th>
+                                    <th scope="col">Horse id</th>
+                                    <th scope="col">Time</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                @foreach ($race->horseRuns as $horse)
+                                    <tr>
+                                        <td>{{ $horse->position }}</td>
+                                        <td>{{ $horse->name }}</td>
+                                        <td>{{ $horse->time }}</td>
+                                    </tr>
+                                @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
         </div>
-
-        <div
-            id="collapse-{{ $race->name }}"
-            class="collapse @if ($loop->first) show @endif"
-            aria-labelledby="heading-{{ $race->name }}"
-            data-parent="#last-races"
-            >
-            <div class="card-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Position</th>
-                            <th scope="col">Horse id</th>
-                            <th scope="col">Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        @foreach ($race->horseRuns as $horse)
-                        <tr>
-                            <td>{{ $horse->position }}</td>
-                            <td>{{ $horse->name }}</td>
-                            <td>{{ $horse->time }}</td>
-                        </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
-            </div>
+    @else
+        <div class="alert alert-primary" role="alert">
+            Results will be shown when at least one race is completed
         </div>
-    </div>
-    @endforeach
-
-</div>
-@else
-<div class="alert alert-primary" role="alert">
-   Results will be shown when at least one race is completed
-</div>
-@endif
-
+    @endif
 
 @endsection
